ca74dafd7cf50e74152601ee3eb1b5497a43bd0c
MUHAMMED YAZEEN A N <muhammedyazeenan@duck.com>
2022-10-31 01:49:36 +0530

    feat added amazonScrapper

--- src/controllers/amazon.py
+++ src/controllers/amazon.py
@@ -1,11 +1,17 @@
 # pylint: disable=missing-module-docstring
 from flask import Response, request
+from autoscraper import AutoScraper
+
 def set_price_list():
     """web-scrap the product details from the given url add it the the database with the userId.
     Parameters:
         url (string): url of the product to be scrapped"""
     if request.method == 'POST':
         url=request.form.get('url')
-
-        return Response(url),200
+        scrapper=AutoScraper()
+        scrapper.load('amazonSearch')
+        result=scrapper.get_result_similar(url,group_by_alias=True)
+        print(type(result))
+        print(result['title'],result['image'],result['price'])
+        return Response("hey"),200
     return Response("invalid"),401
